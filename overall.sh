eb0a2a472c3c8b77127a597ce8af64df55401b8c
Byron Katz <bkatz@costar.com>
2016-02-25 17:26:33 -0500

    making a bit smarter - puts stuff into out and error files

--- overall.sh
+++ overall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # how long to sleep between hitting up each machine.
-SLEEP_TIME=30s
+SLEEP_TIME=60s
 
 MACHINE_ARRAY=(
 "cs01psnddev800" 
@@ -20,16 +20,21 @@ do
 	do
 		# get the info page for Node on each machine, verify it's up and running
 
-		IS_NODE_UP=$(./get_report_info.sh "http://$i:5000/report-server/info" | grep -c "node version" )
+		REPORT_SERVER_INFO=$(./get_report_info.sh "http://$i:5000/report-server/info") 
+		echo $(date) $REPORT_SERVER_INFO
+		IS_NODE_UP=$(echo $REPORT_SERVER_INFO | grep -c "node version" )
 		if test $IS_NODE_UP -eq 0
 		then 
-			echo $(date) "is NodeJS down on $i?"
+			echo $(date) "is NodeJS down on $i?" 1>&2
 		fi
 
-		CPU_STATE=$(./get_cpu_state.sh "US\bkatz"@$i)   # use mpstat and ssh to get cpu state on each machine
+    # use mpstat and ssh to get cpu state on each machine
+		MPSTAT_VALUE=$(ssh "US\bkatz"@$i mpstat 2>/dev/null)
+		echo $(date) $MPSTAT_VALUE
+		CPU_STATE=$(echo $MPSTAT_VALUE | tail -1 | awk '{print $4}' | ./compare_cpu.pl)
 		if test -n "$CPU_STATE"
 			then 
-				echo $(date) $CPU_STATE on $i
+				echo $(date) $CPU_STATE on $i 1>&2
 		fi
 		sleep $SLEEP_TIME
 	done
---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# start the program, sending repetitive unimportant stuff to out.txt
+# and error messages (important stuff) to err.txt.  That way you can see
+# that the program is still running in one, what it's up to, but see
+# the valuable stuff in another file.
+
+./overall.sh > out.txt 2>err.txt
